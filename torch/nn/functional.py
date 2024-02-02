a130748507d8f9a1886e93b547e26c7ad0c12abf
Bfriedel <bfriedel@andrew.cmu.edu>
2024-02-02 09:08:49 -0500

    Add code to check if the reduction mode is sum in pull-request branch

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3318,6 +3318,11 @@ def l1_loss(
 
     See :class:`~torch.nn.L1Loss` for details.
     """
+
+    if reduction == 'sum':
+        raise ValueError("Custom message: Sum reduction mode is not allowed.")
+
+    
     if has_torch_function_variadic(input, target):
         return handle_torch_function(
             l1_loss, (input, target), input, target, size_average=size_average, reduce=reduce, reduction=reduction
